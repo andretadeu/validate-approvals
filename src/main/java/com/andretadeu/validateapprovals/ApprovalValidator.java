9f6e2a97c2567fe52c3d3f5619b09d18093ed036
Andre Tadeu de Carvalho <andre.tadeu.de.carvalho@gmail.com>
2019-01-13 18:32:14 -0200

    Add basic API to test upon.

--- src/main/java/com/andretadeu/validateapprovals/ApprovalValidator.java
+++ src/main/java/com/andretadeu/validateapprovals/ApprovalValidator.java
@@ -1,18 +1,59 @@
 package com.andretadeu.validateapprovals;
 
+import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
 
+/**
+ * Validate approvals fron M approvers to N files
+ */
 public class ApprovalValidator {
     private final Set<String> approvers;
     private final Set<Path> changedFilePaths;
+    private Path root;
 
-    public ApprovalValidator(Set<String> approvers, Set<Path> changedFilePaths) {
-        this.approvers = approvers;
-        this.changedFilePaths = changedFilePaths;
+    public ApprovalValidator(final Set<String> approvers, final Set<Path> changedFilePaths) {
+        this(approvers, changedFilePaths, getDefaultRoot());
     }
 
-    public void validate() {
+    public ApprovalValidator(final Set<String> approvers, final Set<Path> changedFilePaths, final Path root) {
+        Optional.ofNullable(approvers).orElseThrow(new IllegalArgumentExceptionSupplier("approvers"));
+        Optional.ofNullable(changedFilePaths).orElseThrow(new IllegalArgumentExceptionSupplier("changedFilePaths"));
+        this.approvers = approvers;
+        this.changedFilePaths = changedFilePaths;
+        this.root = root == null ? getDefaultRoot() : root;
+    }
 
+    /**
+     * Validate approvals
+     * @return true if all the files have enough approvers, false otherwise.
+     * @throws FileNotFoundException If either {@link #root} is not found or any of the {@link #changedFilePaths} to be
+     *  evaluated are not found.
+     * @throws NotDirectoryException If {@link #root} is not a directory
+     */
+    public boolean validate() throws FileNotFoundException, NotDirectoryException {
+        return false;
+    }
+
+    private static Path getDefaultRoot() {
+        return Paths.get("");
+    }
+
+    private static final class IllegalArgumentExceptionSupplier implements Supplier<IllegalArgumentException> {
+
+        private final String argument;
+
+        IllegalArgumentExceptionSupplier(final String argument) {
+            this.argument = argument;
+        }
+
+        @Override
+        public IllegalArgumentException get() {
+            return new IllegalArgumentException("You must provide a value for '" + argument + "'.");
+        }
     }
 }
--- src/main/java/com/andretadeu/validateapprovals/Main.java
+++ src/main/java/com/andretadeu/validateapprovals/Main.java
@@ -4,6 +4,12 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+
 public class Main {
 
     @Option(name = "-a", aliases = {"--approvers"}, required = true, usage = "List of approvers")
@@ -12,10 +18,17 @@ public class Main {
     @Option(name = "-c", aliases = {"--changed-files"}, required = true, usage = "Changed files for approval")
     private String changedFiles;
 
+    @Option(name = "-r", aliases = {"--root-folder"}, usage = "Repository's root folder")
+    private String rootFolderName;
+
     public static void main(String... args) {
         new Main().doMain(args);
     }
 
+    /**
+     * Effective main method, as an instance method.
+     * @param args arguments passed through console
+     */
     private void doMain(String... args) {
         CmdLineParser parser = new CmdLineParser(this);
 
@@ -25,11 +38,35 @@ public class Main {
             System.err.println(cmdLineEx.getMessage());
             System.err.println(
                     "java -jar dep-mgmt-<versão>.jar --approvers <List of approvers> " +
-                            "--changed-files <Changed files for approval>");
+                            "--changed-files <Changed files for approval> " +
+                            "--root-folder <Repository's root folder>");
             parser.printUsage(System.err);
             System.err.println();
 
             return;
         }
+
+        final String result = validateApprovers(approvers, changedFiles, rootFolderName);
+        if (result != null) {
+            System.out.println(result);
+        }
+    }
+
+    /**
+     *
+     * @param approvers
+     * @param changedFiles
+     * @param rootFolderName
+     * @return
+     */
+    String validateApprovers(final String approvers, final String changedFiles, final String rootFolderName) {
+        Path root = rootFolderName == null || rootFolderName.isEmpty() ? null : Paths.get(rootFolderName);
+        ApprovalValidator approvalValidator = new ApprovalValidator(new HashSet<>(), new HashSet<>(), root);
+        try {
+            return approvalValidator.validate() ? "Approved" : "Insufficient approvals";
+        } catch (FileNotFoundException | NotDirectoryException | IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
     }
 }
